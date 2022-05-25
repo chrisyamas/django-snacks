127dcb69619b9c7f397403c9cf397ae8b554c45f
Christopher Yamas <yamas.chris@gmail.com>
2022-05-25 19:07:18 -0400

    tests modified, README updated

--- snacks/tests.py
+++ snacks/tests.py
@@ -9,17 +9,17 @@ class SnacksTests(SimpleTestCase):
         self.assertEqual(response.status_code, 200)
 
 
+    def test_about_page_status(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        
     def test_home_page_template(self):
         url = reverse('home')
         response = self.client.get(url)
         self.assertTemplateUsed(response, 'home.html')
         self.assertTemplateUsed(response, 'base.html')
-
-
-    def test_about_page_status(self):
-        url = reverse('about')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
 
 
     def test_about_page_template(self):
