900c416b5567c4c94d66215d7807a4994e30c6f2
Christopher Yamas <yamas.chris@gmail.com>
2022-05-26 19:07:48 -0400

    Update settings.py

--- django_snacks_project/settings.py
+++ django_snacks_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)&sknbvj&vj(=u8r967i8jr(@=u2i(w13g%5in^wh#jk5y(3s3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
